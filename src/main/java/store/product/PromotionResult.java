a84f87d0aac013965db843800d26e35efe7b4b9f
armcortex3445 <wlghks5966@naver.com>
2024-11-09 12:26:48 +0900

    feat(Promotion) : add Promotion function
    - manage promotion data
    - provide interface related to accessing promotion data
    - define data structure PromotionResult to communicate between Promotion and the other class

---
+++ src/main/java/store/product/PromotionResult.java
@@ -0,0 +1,40 @@
+package store.product;
+
+
+public class PromotionResult {
+
+    private int appliedItemCount = 0;
+    private int neededItemCount = 0;
+    private PromotionState state;
+
+    public PromotionResult(){
+
+    }
+
+    public PromotionResult (PromotionState state, int appliedItemCount, int neededItemCount){
+        this.state = state;
+        this.appliedItemCount = appliedItemCount;
+        this.neededItemCount = neededItemCount;
+
+    }
+
+//    public void setAppliedItemCount(int appliedItemCount) {
+//        this.appliedItemCount = appliedItemCount;
+//    }
+//
+//    public void setNeededItemCount(int enableItemCount) {
+//        this.neededItemCount = enableItemCount;
+//    }
+
+    public int getAppliedItemCount() {
+        return appliedItemCount;
+    }
+
+    public int getNeededItemCount() {
+        return neededItemCount;
+    }
+
+    public PromotionState getState() { return state; }
+
+
+}
